1653dad09d022764dc4a48f9e37dcec50fff6ed3
Erol Evren <100449806+erlevren@users.noreply.github.com>
2022-12-19 23:58:20 +0300

    18/12/22

--- src/test/java/day01_DriverMethods/C06_ClassWork2.java
+++ src/test/java/day01_DriverMethods/C06_ClassWork2.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class Day01_ClassWork2 {
+public class C06_ClassWork2 {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
